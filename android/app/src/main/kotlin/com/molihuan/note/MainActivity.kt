402f5f91774177dfe787b78d91ab000eaf40a6c8
molihuan <1492906929@qq.com>
2023-10-26 08:22:58 +0800

    更新安卓平台

---
+++ android/app/src/main/kotlin/com/molihuan/note/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.molihuan.note
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/pages/my/view.dart
+++ lib/pages/my/view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:note/common/utils/file_tool.dart';
 import 'package:note/pages/my/ImageGestureResize.dart';
 import 'package:note/pages/my/widgets/DragDividerPage.dart';
@@ -14,7 +16,13 @@ class MyPage extends GetView<MyController> {
 
   // 主视图
   Widget _buildView() {
-    return Text("66");
+    return GFButton(
+        text: "99",
+        onPressed: () {
+          Fluttertoast.showToast(
+            msg: "创建笔记工程失败",
+          );
+        });
   }
 
   @override
